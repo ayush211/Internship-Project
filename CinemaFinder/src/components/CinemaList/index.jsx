ac501d3ce1bf92c057491dd73fe616aaa8734490
Ayush Verma <80851810+ayush211@users.noreply.github.com>
2026-01-07 02:10:17 +0530

    Delete CinemaFinder directory

--- CinemaFinder/src/components/CinemaList/index.jsx
+++
@@ -1,4 +0,0 @@
-import loadable from "@loadable/component";
-
-export const FranchiseCinemasList = loadable(() => import('./FranchiseCinemasList'));
-export const NearbyCinemasList = loadable(() => import('./NearbyCinemasList'));
--- CinemaFinder/src/components/Map/context.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const MapContext = createContext(null);
-export const useMapContext = () => useContext(MapContext);
-
-export const MapContextProvider = MapContext.Provider;
-export default MapContext;
